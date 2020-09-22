7843a2be92177cd61ef7843b1267b945bb29c400
Carl <zeng1090684394@163.com>
2020-09-22 20:16:03 +0800

    Initial local

---
+++ Chapter02/cmake-build-debug/CMakeFiles/Chapter02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chapter02.dir/main.c.o"
+  "Chapter02"
+  "Chapter02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Chapter02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Chapter02/cmake-build-debug/CMakeFiles/Chapter02.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Chapter02.
+# This may be replaced when dependencies are built.
